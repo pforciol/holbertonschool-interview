d80ed06ad0f14f4fbdff79c0965bb253b859d9b5
Pierre Forcioli <2500@holbertonschool.com>
2021-08-23 06:04:22 +0000

    Made task betty compliant

--- 0x05-linked_list_palindrome/0-is_palindrome.c
+++ 0x05-linked_list_palindrome/0-is_palindrome.c
@@ -1,5 +1,13 @@
 #include "lists.h"
 
+/**
+ * is_palindrome - checks if a linked list is a palindrome
+ *
+ * @head: the head address of the linked list
+ *
+ * Return: 1 if it's a palindrome, 0 else
+ */
+
 int is_palindrome(listint_t **head)
 {
 	listint_t *lst = *head;
