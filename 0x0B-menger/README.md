6c8c598b524d16488ece4e97b7a0acbce3785b3b
Pierre Forcioli <2500@holbertonschool.com>
2021-10-07 16:10:44 +0000

    Add all tasks

---
+++ 0x0B-menger/README.md
@@ -0,0 +1,3 @@
+# 0x0B. Menger sponge
+
+-   This is an interview preparation
---
+++ 0x0B-menger/menger.h
@@ -0,0 +1,11 @@
+#ifndef MENGER_H
+#define MENGER_H
+
+#include <math.h>
+#include <stdio.h>
+
+void menger(int level);
+char which_char(int r, int c);
+
+#endif
+
